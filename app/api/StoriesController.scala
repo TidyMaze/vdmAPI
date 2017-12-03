b3ab4bfabfed281183f87ef6112093bb97067405
Yann Rolland <groskiff@gmail.com>
2017-12-03 18:00:11 +0100

    Invalid dates handling

--- app/api/StoriesController.scala
+++ app/api/StoriesController.scala
@@ -14,25 +14,35 @@ import play.api.mvc.ControllerComponents
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
+import scala.util.Try
+import scala.util.Success
+import java.time.format.DateTimeParseException
+import scala.util.Failure
+import java.security.InvalidParameterException
+import scala.concurrent.Future
 
 class StoriesController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
   val storiesDao = new StoriesDAO
 
   def index(from: Option[String], to: Option[String], author: Option[String]) = Action.async {
-    val fromParsed = from.map(d => Instant.parse(d))
-    val toParsed = to.map(d => Instant.parse(d))
-    storiesDao.getStoriesFiltered(fromParsed, toParsed, author).map(stories =>
-      Ok(Json.obj(
-        "posts" -> stories,
-        "count" -> stories.length
-      ))
-    )
+    val fromParsed = from.map(d => Try(Instant.parse(d)))
+    val toParsed = to.map(d => Try(Instant.parse(d)))
+
+    (fromParsed, toParsed) match {
+      case (Some(Failure(e)), _) => Future.successful(BadRequest(s"Invalid parameter from : ${from.get} : ${e.getMessage}"))
+      case (_, Some(Failure(e))) => Future.successful(BadRequest(s"Invalid parameter to : ${to.get} : ${e.getMessage}"))
+      case (paramFrom, paramTo) => storiesDao.getStoriesFiltered(paramFrom.map(_.get), paramTo.map(_.get), author).map(stories =>
+        Ok(Json.obj(
+          "posts" -> stories,
+          "count" -> stories.length)))
+    }
+
   }
 
   def show(id: Int) = Action.async {
     storiesDao.getStoryById(id).map {
       case Some(s) => Ok(Json.obj("post" -> s))
-      case None => NotFound(s"404 : Post with id $id doesn't exist :(")
+      case None    => NotFound(s"404 : Post with id $id doesn't exist :(")
     }
   }
 
