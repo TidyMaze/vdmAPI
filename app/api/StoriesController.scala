8f9bf33cdd3e618b8f41e28f7dd6e7461b2125f6
Yann Rolland <groskiff@gmail.com>
2017-12-03 13:13:34 +0100

    Routing and JSON writing

--- app/api/StoriesController.scala
+++ app/api/StoriesController.scala
@@ -1,17 +1,36 @@
 package api
 
-import play.api.mvc.Controller
 import play.api.mvc.Action
 import play.api.mvc.Result
 import play.api.mvc.BaseController
 import play.api.mvc.ControllerComponents
 import javax.inject.Inject
 import play.api.mvc.AbstractController
+import model.Story
+import java.time.LocalDateTime
+import play.api.libs.json.Json
+import model.Story._
+import play.api.libs.json.JsObject
 
 class StoriesController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok("It works!")
+
+    val obj = Seq(
+      Story(Some(1), Some("Content1"), "author1", LocalDateTime.now()),
+      Story(Some(2), Some("Content2"), "author2", LocalDateTime.now())
+    )
+
+    Ok(Json.obj(
+      "posts" -> obj,
+      "count" -> obj.length))
+  }
+
+  def show(id: Int) = Action {
+    
+    val obj = Story(Some(42), Some("Content"), "author", LocalDateTime.now())
+    
+    Ok(Json.obj("post" -> obj))
   }
 
 }
--- app/model/Story.scala
+++ app/model/Story.scala
@@ -1,6 +1,12 @@
 package model
 
 import java.time.LocalDateTime
+import play.api.libs.json._
 
+object Story {
+  implicit val storyReads = Json.reads[Story]
+  implicit val storyWrites = Json.writes[Story]
+}
 
 case class Story(id: Option[Int], content: Option[String], author: String, date: LocalDateTime)
+
