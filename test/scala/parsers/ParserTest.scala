ef98cd66472702dfaf0f55771aff882ef997a128
Yann Rolland <groskiff@gmail.com>
2017-12-04 13:26:47 +0100

    Fix test

--- test/scala/parsers/ParserTest.scala
+++ test/scala/parsers/ParserTest.scala
@@ -6,13 +6,14 @@ import java.time.format.DateTimeParseException
 import org.scalactic.source.Position.apply
 import java.text.ParseException
 import scraper.VdmParser
+import java.time.Instant
 
 class VdmDateParserTest extends FlatSpec {
 
   val parser = new VdmParser()
   
   "Date parser" should "successfully parse a valid date" in {
-    assert(parser.parseDate("jeudi 30 novembre 2017 17:00") == LocalDateTime.parse("2017-11-30T17:00:00"))
+    assert(parser.parseDate("jeudi 30 novembre 2017 17:00") == Instant.parse("2017-11-30T16:00:00Z"))
   }
 
   it should "produce DateTimeParseException when invalid date is given" in {
@@ -22,7 +23,7 @@ class VdmDateParserTest extends FlatSpec {
   }
   
   "MetaParser" should "successfully parse a full meta row" in {
-    assert(parser.parseMeta("Par Djo - / jeudi 30 novembre 2017 11:30 / France") == ("Djo", LocalDateTime.parse("2017-11-30T11:30")))
+    assert(parser.parseMeta("Par Djo - / jeudi 30 novembre 2017 11:30 / France") == ("Djo", Instant.parse("2017-11-30T10:30:00Z")))
   }
 
   it should "produce ParseException when invalid meta is given" in {
